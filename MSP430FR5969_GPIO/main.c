ff31ace3e35dac4387120b24d5ba24d8fdfe58e6
Jisu Kim <jisu891114@hotmail.com>
2021-03-09 02:25:44 +0900

    [ADD] GPIO TEST

--- MSP430FR5969_GPIO/main.c
+++ MSP430FR5969_GPIO/main.c
@@ -9,12 +9,11 @@ int main(void)
 	
 	PM5CTL0 &= ~LOCKLPM5; // Unlock port... The FRAM devices lock port status at power-up until you unlock the ports.
 
-    P1DIR |= (1<<LED_GRN); // P1.0 output setting.
-    P1OUT |= (1<<LED_GRN); // Turn on the green led.
+	P1DIR |= (1<<LED_GRN); // P1.0 output setting.
+	P1OUT |= (1<<LED_GRN); // Turn on the green led.
 
 	P4DIR |= (1<<LED_RED); // P4.6 output setting.
 	P4OUT |= (1<<LED_RED); // Turn on the red led.
-
 
 	P1OUT &= ~(1<<LED_GRN); // Turn off the green led.
 	P4OUT &= ~(1<<LED_RED); // Turn off the red led.
